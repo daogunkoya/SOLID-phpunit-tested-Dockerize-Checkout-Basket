addb1f59b68569bb4e7397a1ba63afb4c0fea411
Daniel Ogunkoya <da.ogunkoya@gmail.com>
2023-02-02 07:21:02 +0000

    update test on basket

--- tests/BasketTest.php
+++ tests/BasketTest.php
@@ -3,10 +3,22 @@
 namespace App;
 
 use PHPUnit\Framework\TestCase;
-use App\User;
+use App\Controllers\Home;
+use App\Models\product;
 
-final class UserTest extends TestCase
+final class BasketTest extends TestCase
 {
 
+
+    public function testAddToBasket()
+    {
+        $home = new Home;
+        $products = new Product;
+        $productList = $products->getProducts();
+
+        $this->assertIsArray($home->init(8, $productList));
+        // $this->assertContains('Avengers', $user->favorite_movies);
+        // $this->assertCount(1, $user->favorite_movies);
+    }
 
 }
