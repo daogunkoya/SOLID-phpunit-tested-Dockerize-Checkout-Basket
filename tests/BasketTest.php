b1baefd29c8e48d16a7d09586bed6dbe274ecd5b
Daniel Ogunkoya <da.ogunkoya@gmail.com>
2023-02-04 10:21:05 +0000

    update BasketTest

--- tests/BasketTest.php
+++ tests/BasketTest.php
@@ -5,20 +5,56 @@ namespace App;
 use PHPUnit\Framework\TestCase;
 use App\Controllers\Home;
 use App\Models\product;
+use App\Features\DiscountedBasket;
+use App\Features\RegularBasket;
+use App\Services\BasketService;
 
 final class BasketTest extends TestCase
 {
 
 
-    public function testAddToBasket()
+    public function testBasketInit()
     {
         $home = new Home;
         $products = new Product;
         $productList = $products->getProducts();
 
-        $this->assertIsArray($home->init(8, $productList));
-        // $this->assertContains('Avengers', $user->favorite_movies);
-        // $this->assertCount(1, $user->favorite_movies);
+        $res = $home->init(8, $productList);
+        
+        
+        $this->assertIsArray($res);
+    
+         $this->assertCount(2, $res);
     }
+
+
+    public function testAddMethodForBasket()
+    {
+        $regularBasket = new RegularBasket;
+    
+        $regularBasket->addToBasket('P001');
+        $regularBasket->addToBasket('P002');
+
+        $this->assertEquals($regularBasket->getBasket(), ['P001', 'P002']);
+        
+    
+    }
+    public function testTotalPrice()
+    {
+        $regularBasket = new RegularBasket;
+        $products = new Product;
+        $productList = $products->getProducts();
+       
+        $productP001 = $productList['P001'];
+        $productP002 = $productList['P002'];
+
+        $regularBasket->addToBasket('P001');
+        $regularBasket->addToBasket('P002');
+
+        $this->assertEquals($regularBasket->fetchTotal($productList),  $productP001['price'] + $productP002['price']);
+        
+       
+    }
+   
 
 }
